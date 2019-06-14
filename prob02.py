c439790a93c49bb64c65d4fc53f927db52f01ccd
rkskekabc <shinde13@naver.com>
2019-06-14 17:47:11 +0900

    문제2

--- prob02.py
+++ prob02.py
@@ -1,8 +1,19 @@
 # range() 함수와 유사한 frange() 함수를 작성해 보세요. frange() 함수는 실수 리스트를 반환합니다.
 
 
-def frange(val, start=0.0, step=0.1):
-    pass
+def frange(val, basic=0.0, step=0.1):
+    returnlist = []
+    if basic < val:
+        startnum = basic
+        endnum = val
+    else:
+        startnum = val
+        endnum = basic
+    while startnum < endnum:
+        returnlist.append(round(startnum, 1))
+        startnum += step
+
+    return returnlist
 
 
 print(frange(2))
